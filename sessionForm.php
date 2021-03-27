94ca265a5d111a64ca008e4e531a65fab65e5dac
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-03-27 15:17:54 -0600

    Updates

--- sessionForm.php
+++ sessionForm.php
@@ -12,21 +12,35 @@
             <input type="password" name="password" /><br/><br/>
             <label>FECHA DE NACIMIENTO</label>
             <input type="date" name="fecha" /><br/><br/>
-            <input type = 'submit' name = 'submit' value = Login> 
+            <label>Lenguaje de programación que domina (selección múltiple)</label>
+            <select name="languaje[]" multiple>
+                <option value="JavaScript">JavaScript</option>
+                <option value="TypeScript">TypeScript</option>
+                <option value="PHP">PHP</option>
+                <option value="Java">Java</option>
+                <option value="Python">Python</option>
+            <option value="Go">Go</option>
+        </select><br/><br/>
+        <input type = 'submit' name = 'submit' value = Login>
         </form> 
     </body> 
 </html> 
 <?php 
     session_start();
 
-
     if(isset($_POST["submit"]))  
     { 
-        if(isset($_POST["nombre"]) && $_POST["password"] != '' && $_POST["email"] != '' && $_POST["fecha"] != '')  
+        if(isset($_POST["nombre"]) && $_POST["password"] != '' && $_POST["email"] != '' && $_POST["fecha"] != '' && isset($_POST["languaje"]))  
         { 
-            $user = array("nombre"=>$_POST["nombre"], "password"=>$_POST["password"], "email"=>$_POST["email"], "fecha"=>$_POST["fecha"]);
+            $comma_separated = implode(", ", $_POST['languaje']);
+            $user = array("nombre"=>$_POST["nombre"], "password"=>$_POST["password"], "email"=>$_POST["email"], "fecha"=>$_POST["fecha"], "languajes"=>$comma_separated);
             setcookie("inicio", time(), time() + (86400 * 30), "/");
             $_SESSION["user"] = $user ;
+            if(isset($_SESSION["users"])) {
+                $_SESSION["users"][$user["nombre"]] = $user;
+            } else {
+                $_SESSION["users"][$user["nombre"]] = $user;
+            }
         } 
     else
         echo "Por favor llenar todos los campos !!"; 
--- showUserData.php
+++ showUserData.php
@@ -6,7 +6,7 @@
         ?>
         <p>Han pasado <?=time() - $_COOKIE["inicio"]?> segundos desde que se inició sesión.</p>
         <p>Nombre:<?=$_SESSION["user"]["nombre"]?>, Email:<?=$_SESSION["user"]["email"]?>, Password:<?=$_SESSION["user"]["password"]?>,
-        Fecha de nacimiento:<?=$_SESSION["user"]["fecha"]?>, la sesión sigue activa.</p>
+        Fecha de nacimiento:<?=$_SESSION["user"]["fecha"]?>, Lenguajes: <?=$_SESSION["user"]["languajes"]?>. La sesión sigue activa.</p>
         <button onclick="location.href='salir.php'" type="button">Cerrar sesión</button>
         <?php
     }
