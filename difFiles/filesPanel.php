0e1f39fa5047d867c8bb3553c47268df23e79d90
Edgar Gabriel Martinez Gonzalez <edgarggamartgo@gmail.com>
2021-05-15 13:02:39 -0500

    Actividad Integradora. Respuestas en PHP

---
+++ difFiles/filesPanel.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+    <form method='post'>
+        <h1>PANEL GENERADOR DE ARCHIVOS</h1>
+        <br />
+        <label>DESCARGA ARCHIVO WORD</label>
+        <a href="wordFile.php">WORD</a><br /><br />
+        <label>DESCARGA ARCHIVO XLSX</label>
+        <a href="excelFile.php">XLSX</a><br /><br />
+        <label>DESCARGA ARCHIVO PDF</label>
+        <a href="pdfFile.php">PDF</a><br /><br />
+        <label>DESCARGA ARCHIVO IMG</label>
+        <a href="img.php">IMG</a><br /><br />
+    </form>
+</body>
+
+</html>
